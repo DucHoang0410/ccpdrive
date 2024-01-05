b07f721d93a2ee5e7877935830d414db9e81f01b
Nguyễn Đức Hoàng <82095602+DucHoang0410@users.noreply.github.com>
2024-01-05 04:54:42 +0000

    demo

---
+++ cppdrive/build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/ccpdrive/cppdrive/src/authenticate.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/authenticate.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/cd.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/cd.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/command.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/command.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/download.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/download.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/find.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/find.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/ls.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/ls.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/pwd.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/pwd.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/quit.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/quit.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/share.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/share.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/command/upload.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/command/upload.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/connect.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/connect.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/crypto.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/crypto.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/log.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/log.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/message.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/message.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/status.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/status.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/utils.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/utils.cpp.o"
+  "/workspaces/ccpdrive/cppdrive/src/validate.cpp" "/workspaces/ccpdrive/cppdrive/build/CMakeFiles/engine.dir/src/validate.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cppdrive/build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/src/authenticate.cpp.o"
+  "CMakeFiles/engine.dir/src/command/cd.cpp.o"
+  "CMakeFiles/engine.dir/src/command/command.cpp.o"
+  "CMakeFiles/engine.dir/src/command/download.cpp.o"
+  "CMakeFiles/engine.dir/src/command/find.cpp.o"
+  "CMakeFiles/engine.dir/src/command/ls.cpp.o"
+  "CMakeFiles/engine.dir/src/command/pwd.cpp.o"
+  "CMakeFiles/engine.dir/src/command/quit.cpp.o"
+  "CMakeFiles/engine.dir/src/command/share.cpp.o"
+  "CMakeFiles/engine.dir/src/command/upload.cpp.o"
+  "CMakeFiles/engine.dir/src/connect.cpp.o"
+  "CMakeFiles/engine.dir/src/crypto.cpp.o"
+  "CMakeFiles/engine.dir/src/log.cpp.o"
+  "CMakeFiles/engine.dir/src/message.cpp.o"
+  "CMakeFiles/engine.dir/src/status.cpp.o"
+  "CMakeFiles/engine.dir/src/utils.cpp.o"
+  "CMakeFiles/engine.dir/src/validate.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
